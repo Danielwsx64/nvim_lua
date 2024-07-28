3f69bdd5e0df6714d1024575f2ba48afcdc78e42
Danielwsx64 <danielws.x64@gmail.com>
2024-07-28 09:02:30 -0300

    adjust autoformat

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -26,6 +26,7 @@ return {
 			javascript = { "prettier" },
 			typescriptreact = { "prettier" },
 			typescrip = { "prettier" },
+			elixir = { "mix" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
@@ -34,4 +35,17 @@ return {
 			-- javascript = { { "prettierd", "prettier" } },
 		},
 	},
+	init = function()
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_format = "fallback", range = range })
+		end, { range = true })
+	end,
 }
